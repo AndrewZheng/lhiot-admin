70edd4fba8bad2c3fa9be2e8729a37e83d5c68ae
andrew.zheng <andy_520123@163.com>
2018-10-09 16:27:17 +0800

    1、[A] 增加validate函数集 2、[D] 删除不必要的注释代码

--- src/router/index.js
+++ src/router/index.js
@@ -38,20 +38,12 @@ router.beforeEach((to, from, next) => {
           }
           store.dispatch('getRouteListById', pid).then(res => {
             console.log('getActualRouter: ', store.getters.getActualRouter);
-            router.addRoutes(store.getters.getActualRouter); // 动态添加可访问路由表
+            router.addRoutes(store.getters.getActualRouter);
             next({ ...to, replace: true });
           });
         });
       } else {
         console.log('to.path: ', to.path);
-        // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;
-        // if (store.getters.getRoutePermission[to.path]) next(); // 有权限，可访问
-        // else {
-        //   next({
-        //     replace: true,
-        //     name: 'error_401'
-        //   });
-        // } // 无权限，重定向到401页面
         next();
       }
     }
@@ -62,7 +54,7 @@ router.beforeEach((to, from, next) => {
       next();
     } else {
       next({
-        name: LOGIN_PAGE_NAME // 否则全部定向登录页
+        name: LOGIN_PAGE_NAME
       });
     }
   }
--- src/store/module/user.js
+++ src/store/module/user.js
@@ -108,10 +108,6 @@ const actions = {
       }).catch(err => {
         reject(err);
       });
-      // 如果你的退出登录无需请求接口，则可以直接使用下面三行代码而无需使用logout调用接口
-      // commit('setToken', '')
-      // commit('setAccess', [])
-      // resolve()
     });
   },
   // 获取用户相关信息
