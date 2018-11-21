50f3e0c3bf9012f438a490acd429f0fde81ee5a8
Jerry <448749903@qq.com>
2018-11-21 12:11:43 +0800

    后台管理 内容管理、页面板块管理、定置管理、订单管理、规则参数管理、活动管理界面编写 以及mock数据填充(点击事件未完成)

--- src/view/fruit-master-manager/custom/custom-plan.vue
+++ src/view/fruit-master-manager/custom/custom-plan.vue
@@ -14,14 +14,10 @@
         <div slot="searchCondition">
           <Row>
             <Col span="24">
-            <Input placeholder="广告名称" class="search-input" v-model="searchRowData.name" style="width: auto"/>
-            <Select v-model="rowData.status" class="search-col ml15"  placeholder="位置类型">
-              <Option v-for="item in advertisementList" :value="item.value" class="mb10 ml15" :key="`search-col-${item.value}`">{{item.value}}</Option>
-            </Select>
+            <Input placeholder="定制计划名称" class="search-input" v-model="searchRowData.name" style="width: auto"/>
             <Button v-waves @click="handleSearch" class="search-btn ml20" type="primary">
               <Icon type="md-search"/>&nbsp;搜索
             </Button>
-
             <Button v-waves type="success" class="ml5 mr5" @click="addChildren">
               <Icon type="md-add"/>
               创建
@@ -29,6 +25,10 @@
             <Button v-waves type="error" class="ml5 mr5" @click="deleteChildren">
               <Icon type="md-close"/>
               删除
+            </Button>
+            <Button v-waves type="primary" class="ml5 mr5" @click="exportExcel">
+              <Icon type="md-download"/>
+              导出
             </Button>
             </Col>
           </Row>
@@ -220,58 +220,69 @@
     },
     data() {
       return {
-        advertisementList: [],
         columns: [
           {
-            title: 'ID',
+            title: '定制计划主图',
             key: 'id',
             sortable: true,
             width: 180,
             fixed: 'left'
           },
           {
-            title: '广告图片',
+            title: '定制计划名称',
             key: 'name',
             width: 150
           },
           {
-            title: '广告位置',
+            title: '价格(周/单人)',
             width: 150,
             key: 'onSaleName'
           },
           {
-            title: '广告描述',
+            title: '价格(周/双人)',
             width: 150,
             key: 'onSaleRole'
           },
           {
-            title: '广告类型',
+            title: '价格(周/三人)',
             width: 150,
             key: 'roleCode'
           },
           {
-            title: '链接类型',
+            title: '价格(月/单人)',
             width: 180,
             key: 'price',
             sortable: true
           },
           {
-            title: '广告状态',
+            title: '价格(月/双人)',
             width: 150,
             key: 'sort',
             sortable: true
           },
           {
-            title: '有效时间',
+            title: '价格(月/三人)',
             width: 150,
             key: 'price',
             sortable: true
           },
           {
+            title: '排序',
+            width: 150,
+            key: 'onSale',
+            sortable: true
+          },
+          {
+            title: '是否上架',
+            width: 150,
+            key: 'onSale',
+            sortable: true
+          },
+          {
             title: '操作',
-            minWidth: 200,
+            minWidth: 250,
             key: 'handle',
-            options: ['delete', 'edit', 'view', 'onSale']
+            options: ['delete', 'edit', 'view', 'onSale', 'settings']
           }
         ],
         tableData: [],
