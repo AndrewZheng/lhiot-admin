7f85e6b3d4d804c5ceacf8c589f6ca6b92fd848e
Jerry <448749903@qq.com>
2019-01-11 17:30:40 +0800

    登录首页 记住用户账号密码功能实现

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -7,7 +7,7 @@
     <div class="login-con">
       <div class="login-con-header">
         <div class="login-con-header-left">
-          <img :src="maxLogo">绿航物联
+          <img :onerror="logo" src="../../../src/assets/images/lhiot_logo.jpg" >绿航物联
         </div>
         <div style="color: black">|</div>
         <div class="login-con-header-right">云平台管理</div>
@@ -30,7 +30,8 @@ export default {
   },
   data() {
     return {
-      maxLogo
+      maxLogo,
+      logo: 'this.src="' + require('../../../src/assets/images/lhiot_logo.jpg') + '"'
     }
   },
   methods: {
